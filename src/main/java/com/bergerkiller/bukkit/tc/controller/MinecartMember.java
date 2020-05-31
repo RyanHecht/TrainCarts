7107027a13fbb3755ec1e819b51fc71e29ae99ab
Ryan Hecht <ryanhecht.tse@gmail.com>
2020-05-31 18:11:47 -0400

    Revert "Add some very basic minecart-minecart collision code (so that train collision default works)"
    This reverts commit 5d459dab8f61993e969ed80bfc9e79af480eb17d.

--- src/main/java/com/bergerkiller/bukkit/tc/controller/MinecartMember.java
+++ src/main/java/com/bergerkiller/bukkit/tc/controller/MinecartMember.java
@@ -1348,22 +1348,8 @@ public abstract class MinecartMember<T extends CommonMinecart<?>> extends Entity
             return;
         }
 
-        // When bumping with other minecarts we should technically do the whole vanilla train mechanic
-        // Instead for simplicity's sake we're executing fairly vanilla behavior
+        // Ignore other minecarts completely. They're going to be our trains anyway.
         if (e instanceof Minecart) {
-            Vector pos_diff = e.getLocation().subtract(entity.getLocation()).toVector();
-            double len_sq = pos_diff.lengthSquared();
-            if (len_sq >= 1.0e-4) {
-                double n = MathUtil.getNormalizationFactorLS(len_sq);
-                pos_diff.multiply(n);
-                if (n > 1.0) {
-                    n = 1.0;
-                }
-                pos_diff.multiply(0.05 * n);
-                applyBump(e, pos_diff);
-                applyBump(entity.getEntity(), pos_diff.multiply(-1.0));
-            }
-
             return;
         }
 
