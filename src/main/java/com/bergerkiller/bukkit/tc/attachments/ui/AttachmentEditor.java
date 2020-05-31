1b9faf982e08728b5eb414c8a6b7301c29319c90
Ryan Hecht <ryanhecht.tse@gmail.com>
2020-05-31 18:11:47 -0400

    Revert "Fix NPE refreshing model when attachment editor disables"
    This reverts commit f343c135666b8280f382cf81c61be252ea074be3.

--- src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/AttachmentEditor.java
+++ src/main/java/com/bergerkiller/bukkit/tc/attachments/ui/AttachmentEditor.java
@@ -108,9 +108,6 @@ public class AttachmentEditor extends MapDisplay {
 
     @Override
     public void onStatusChanged(MapStatusEvent event) {
-        if (this.tree.getDisplay() == null) {
-            return; // Detached, do nothing
-        }
         if (event.isName("changed") || event.isName("changed_silent")) {
             MapWidgetAttachmentNode node = event.getArgument(MapWidgetAttachmentNode.class);
             if (node != null) {
