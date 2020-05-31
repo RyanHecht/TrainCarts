c81f4cc69104c20ac7a2c5a9c1a3eb495cb3a560
Ryan Hecht <ryanhecht.tse@gmail.com>
2020-05-31 18:11:47 -0400

    Revert "Setup Economy in TrainCarts@enable"
    This reverts commit 9cfa68ae50c508304f2ea02902bf357432a0eacf.

--- src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
+++ src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
@@ -378,9 +378,6 @@ public class TrainCarts extends PluginBase {
         attachmentModels = new AttachmentModelStore(getDataFolder() + File.separator + "models.yml");
         attachmentModels.load();
 
-        //Setup Economy (Vault)
-        setupEconomy();
-
         //init statements
         Statement.init();
 
