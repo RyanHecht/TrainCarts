0d29d9eaeee97cdd626369099feaa2bdef64c1c4
Ryan Hecht <ryanhecht.tse@gmail.com>
2020-05-31 18:11:47 -0400

    Revert "Allow spawning a single-cart train on a single piece of track"
    This reverts commit 731ac495e480d6deb8e328ab435e14c418ad7681.

--- src/main/java/com/bergerkiller/bukkit/tc/utils/StoredTrainItemUtil.java
+++ src/main/java/com/bergerkiller/bukkit/tc/utils/StoredTrainItemUtil.java
@@ -206,10 +206,6 @@ public class StoredTrainItemUtil {
 
         // Find locations to spawn at
         List<Location> locations = SignActionSpawn.getSpawnPositions(spawnLoc, false, orientation, group.getMembers());
-        if (locations.isEmpty() && group.getMembers().size() == 1) {
-            // Spawn at the exact location
-            locations.add(spawnLoc);
-        }
         if (locations.size() < group.getMembers().size()) {
             return SpawnResult.FAIL_BLOCKED;
         }
