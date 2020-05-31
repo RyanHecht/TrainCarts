325465791a99d7a25f2d904fc9cdb9d2339598c4
Ryan Hecht <ryanhecht.tse@gmail.com>
2020-05-31 18:11:47 -0400

    Revert "Set econ defaults vaulu to null"
    This reverts commit ccd2e337101c30bd6d1c6bc1ffafbc469d5b7b50.

--- src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
+++ src/main/java/com/bergerkiller/bukkit/tc/TrainCarts.java
@@ -76,7 +76,7 @@ public class TrainCarts extends PluginBase {
     private RedstoneTracker redstoneTracker;
     private GlowColorTeamProvider glowColorTeamProvider;
     private PathProvider pathProvider;
-    private static Economy econ = null;
+    private static Economy econ;
 
     /**
      * Gets a helper class for assigning (fake) entities to teams to change their glowing effect
